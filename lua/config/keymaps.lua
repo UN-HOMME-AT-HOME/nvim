2943f2adf1a74b7c9c7c01f984567c9239344726
Un homme at home <45526591+UN-HOMME-AT-HOME@users.noreply.github.com>
2024-12-04 00:43:52 -0800

    keymaps update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,28 +2,32 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "<S-y>", function()
-  return '"+y'
+	return '"+y'
 end, { expr = true })
 
 -- windows
 vim.keymap.set("n", "<leader>l", function()
-  return "<C-w>l"
+	return "<C-w>l"
 end, { expr = true })
 
 vim.keymap.set("n", "<leader>h", function()
-  return "<C-w>h"
+	return "<C-w>h"
 end, { expr = true })
 
 vim.keymap.set("n", "<leader>j", function()
-  return "<C-w>s"
+	return "<C-w>s"
 end, { expr = true })
 
 vim.keymap.set("n", "<leader>c", function()
-  return "<C-w>c"
+	return "<C-w>c"
 end, { expr = true })
 
 vim.keymap.set("n", "<leader>g", function()
-  return "Gi<CR>"
+	return "Gi<CR>"
+end, { expr = true })
+
+vim.keymap.set("n", "<leader>w", function()
+	return ":w<CR>"
 end, { expr = true })
 
 -- todo make :hs do CWs
